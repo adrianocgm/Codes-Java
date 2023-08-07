a70cb6fb311b14c12be075e462f7ae49ea5a2a9c
Adriano Menezes <adrianocgm@gmail.com>
2023-08-07 19:08:26 -0300

    Program to calculate age

---
+++ age_calculator/src/age_calculator/AgeCalculator.java
@@ -0,0 +1,33 @@
+package age_calculator;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Scanner;
+
+public class AgeCalculator {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter your name: ");
+		String name = scanner.nextLine();
+		
+		System.out.println("Enter your brith date 'AAAA-MM-DD' : ");
+		String birthDate = scanner.nextLine();
+		LocalDate birthDateLocal = LocalDate.parse(birthDate);
+		
+		ZoneId zoneId = ZoneId.of("America/Sao_Paulo");
+		LocalDate today = LocalDate.now(zoneId);
+		
+		Period period = Period.between(birthDateLocal, today);
+		
+		int age = period.getYears();
+		
+		System.out.println("Hello!! " + name.toUpperCase() + " your age is " + age + ".");
+		
+		scanner.close();		
+
+	}
+
+}
